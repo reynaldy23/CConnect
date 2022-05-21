8e91ef06dcb435254af930bb1c2aa56bdbb4e4ee
Reynaldy <reynaldykwok@gmail.com>
2022-05-22 02:50:25 +0800

    onboarding base

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,53 +15,79 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
+
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final controller = PageController();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  void dispose(){
+    controller.dispose();
+
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Container(
+        padding: const EdgeInsets.only(bottom: 80),
+        child: PageView(
+          controller: controller,
+          // scrollDirection: Axis.vertical,
+          children: [
+            Container(
+              color: Colors.red,
+              child: const Center(child: Text('Page 1')),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Container(
+              color: Colors.indigo,
+              child: const Center(child: Text('Page 2')),
             ),
+            Container(
+              color: Colors.green,
+              child: const Center(child: Text('Page 3')),
+            ),
+
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomSheet: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 0),
+        height: 80,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            TextButton(onPressed: (){controller.jumpToPage(2);}, child: const Text('SKIP')),
+            Center(child: SmoothPageIndicator(
+              controller: controller,
+                count: 3,
+              effect: WormEffect(
+                spacing: 16,
+                dotColor: Colors.black26,
+                activeDotColor: Colors.teal.shade700,
+              ),
+              onDotClicked: (index) => controller.animateToPage(
+                index,
+                duration: const Duration(milliseconds: 200),
+                curve: Curves.easeIn,
+              ),
+            )),
+            TextButton(onPressed: (){controller.nextPage(duration: const Duration(milliseconds: 200), curve: Curves.easeInOut);}, child: const Text('NEXT'))
+          ],
+        ),
+      ),
     );
   }
 }
