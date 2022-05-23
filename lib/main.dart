f89be830462da0d7f691b2fc81d26c770ed9ee18
Reynaldy <reynaldykwok@gmail.com>
2022-05-24 00:41:20 +0800

    onboarding checkpoint #2

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ms_undraw/ms_undraw.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -47,56 +48,125 @@ class _MyHomePageState extends State<MyHomePage> {
           controller: controller,
           // scrollDirection: Axis.vertical,
           children: [
-            Container(
+            Container( //pg1
               color: const Color(0xFFFFFFFF),
               child: Column(
                 children: [
                   UnDraw(
-                    padding: const EdgeInsets.only(top: 100, bottom: 70),
+                    padding: const EdgeInsets.only(top: 100, bottom: 75),
                     color: Colors.blue,
-                    height: 260,
-                    width: 260,
-                    illustration: UnDrawIllustration.profile_image,
+                    height: 240,
+                    width: 240,
+                    illustration: UnDrawIllustration.profile_data,
                     placeholder: const CircularProgressIndicator(),
                     errorWidget: const Icon(Icons.error_outline,
                         color: Colors.red, size: 50),
                   ),
                   Container(
-                      padding: const EdgeInsets.only(bottom: 25),
-                      child: const Text("Welcome To CConnect")),
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: Text("Welcome To CConnect",
+                        style: GoogleFonts.poppins(
+                            fontSize: 25,
+                            color: const Color(0xFF4285F4),
+                            fontWeight: FontWeight.bold)),
+                  ),
                   const Text(
-                    "CConnect helps you to organize and \n share your social media account\n like Youtube, Instagram, Line,\nTwitter and others seamlessly!",
-                    textAlign: TextAlign.justify,
-                  )
+                    "CConnect helps you to organize and\nshare your social media account\nlike Youtube, Instagram, Line,\nTwitter and others seamlessly!",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 15),
+                  ),
                 ],
               ),
             ),
-            Container(
+            Container( //pg2
               color: const Color(0xFFFFFFFF),
-              child: Center(
-                child: UnDraw(
-                  color: Colors.blue,
-                  height: 280,
-                  width: 280,
-                  illustration: UnDrawIllustration.profile_image,
-                  placeholder: const CircularProgressIndicator(),
-                  errorWidget: const Icon(Icons.error_outline,
-                      color: Colors.red, size: 50),
-                ),
+              child: Column(
+                children: [
+                  UnDraw(
+                    padding: const EdgeInsets.only(top: 155, bottom: 60),
+                    color: Colors.blue,
+                    height: 200,
+                    width: 200,
+                    illustration: UnDrawIllustration.button_style,
+                    placeholder: const CircularProgressIndicator(),
+                    errorWidget: const Icon(Icons.error_outline,
+                        color: Colors.red, size: 50),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: Text("Organize all your contacts\nthe way you wanted!",
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.poppins(
+                            fontSize: 25,
+                            color: const Color(0xFF4285F4),
+                            fontWeight: FontWeight.bold)),
+                  ),
+                  const Text(
+                    "With CConnect, any type of social\nmedia account can be stored inside\nCConnect and ready to share\nor receive contacts anytime!",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ],
               ),
             ),
-            Container(
+            Container( //pg3
               color: const Color(0xFFFFFFFF),
-              child: Center(
-                child: UnDraw(
-                  color: Colors.blue,
-                  height: 280,
-                  width: 280,
-                  illustration: UnDrawIllustration.profile_image,
-                  placeholder: const CircularProgressIndicator(),
-                  errorWidget: const Icon(Icons.error_outline,
-                      color: Colors.red, size: 50),
-                ),
+              child: Column(
+                children: [
+                  UnDraw(
+                    padding: const EdgeInsets.only(top: 100, bottom: 75),
+                    color: Colors.blue,
+                    height: 240,
+                    width: 240,
+                    illustration: UnDrawIllustration.connected_world,
+                    placeholder: const CircularProgressIndicator(),
+                    errorWidget: const Icon(Icons.error_outline,
+                        color: Colors.red, size: 50),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: Text("Lost your device? No Problem!",
+                        style: GoogleFonts.poppins(
+                            fontSize: 25,
+                            color: const Color(0xFF4285F4),
+                            fontWeight: FontWeight.bold)),
+                  ),
+                  const Text(
+                    "CConnect will store all your saved\ncontacts in the cloud, so you can\naccess them by using your account.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ],
+              ),
+            ),
+            Container( //pg4
+              color: const Color(0xFFFFFFFF),
+              child: Column(
+                children: [
+                  UnDraw(
+                    padding: const EdgeInsets.only(top: 155, bottom: 60),
+                    color: Colors.blue,
+                    height: 200,
+                    width: 200,
+                    illustration: UnDrawIllustration.account,
+                    placeholder: const CircularProgressIndicator(),
+                    errorWidget: const Icon(Icons.error_outline,
+                        color: Colors.red, size: 50),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: Text("Let's Get Started!",
+                        style: GoogleFonts.poppins(
+                            fontSize: 25,
+                            color: const Color(0xFF4285F4),
+                            fontWeight: FontWeight.bold)),
+                  ),
+                  const Text(
+                    "Create your account right now\nto enjoy all CConnect benefits!",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ],
               ),
             ),
           ],
@@ -111,13 +181,13 @@ class _MyHomePageState extends State<MyHomePage> {
           children: [
             TextButton(
                 onPressed: () {
-                  controller.jumpToPage(2);
+                  controller.jumpToPage(3);
                 },
                 child: const Text('SKIP')),
             Center(
               child: SmoothPageIndicator(
                 controller: controller,
-                count: 3,
+                count: 4,
                 effect: WormEffect(
                   spacing: 16,
                   dotColor: Colors.black26,
